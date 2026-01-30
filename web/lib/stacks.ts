591a46647911dfe3c9b4a79985c05aac478503e1
manny <praisechinonso21@gmail.com>
2026-01-30 10:20:59 +0100

    fix: update network imports for v7

--- web/lib/stacks.ts
+++ web/lib/stacks.ts
@@ -1,8 +1,8 @@
-import { StacksTestnet, StacksMainnet } from '@stacks/network';
+import { STACKS_TESTNET, STACKS_MAINNET } from '@stacks/network';
 
 export const network = process.env.NEXT_PUBLIC_STACKS_NETWORK === 'mainnet'
-  ? new StacksMainnet()
-  : new StacksTestnet();
+  ? STACKS_MAINNET
+  : STACKS_TESTNET;
 
 export const explorerUrl = (txId: string) =>
   `https://explorer.hiro.so/txid/${txId}?chain=${process.env.NEXT_PUBLIC_STACKS_NETWORK || 'testnet'}`;
